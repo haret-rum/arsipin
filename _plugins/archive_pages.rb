3e7bec26ecd1f7140de116666855c8d13457d999
segara-alam <abimanyfaishol@gmail.com>
2026-08-28 09:36:06 +0300

    major tag, author, category archive

--- _plugins/archive_pages.rb
+++ _plugins/archive_pages.rb
@@ -1,4 +1,4 @@
-class ArchivePage < Jekyll::Page
+class ArchivePage < Jekyll::PageWithoutAFile
   def initialize(site, dir, data)
     super(site, site.source, dir, "index.html")
     data.each { |key, value| self.data[key] = value }
@@ -13,17 +13,32 @@ class ArchivePagesGenerator < Jekyll::Generator
     per_page = site.config.fetch("paginate", 7).to_i
     per_page = 1 if per_page < 1
 
-    generate_taxonomy_pages(site, "categories", site.categories, per_page)
-    generate_taxonomy_pages(site, "tags", site.tags, per_page)
-    generate_all_tags_page(site, per_page)
+    category_archive_type = site.config.dig("category_archive", "type")
+    unless category_archive_type == "jekyll-archives"
+      generate_taxonomy_pages(site, "categories", site.categories, per_page)
+    end
+
+    tag_archive_type = site.config.dig("tag_archive", "type")
+    unless tag_archive_type == "jekyll-archives"
+      generate_taxonomy_pages(site, "tags", site.tags, per_page)
+      generate_all_tags_page(site, per_page)
+    end
+
+    generate_archive_pagination_pages(site, "tags", site.tags, per_page) if tag_archive_type == "jekyll-archives"
+    generate_archive_pagination_pages(site, "categories", site.categories, per_page) if category_archive_type == "jekyll-archives"
+
     generate_author_pages(site, per_page)
   end
 
   private
 
   def generate_taxonomy_pages(site, type, taxonomies, per_page)
+    return unless taxonomies.is_a?(Hash)
+
     taxonomies.each do |name, documents|
-      posts = visible_posts(documents)
+      next if name.nil? || name.to_s.strip.empty?
+
+      posts = visible_posts(Array(documents))
       archive_data = {
         "layout" => "archive-taxonomy",
         "title" => name.to_s,
@@ -36,10 +51,14 @@ class ArchivePagesGenerator < Jekyll::Generator
 
   def generate_all_tags_page(site, per_page)
     posts = visible_posts(site.posts.docs)
+    tag_names = site.tags.to_h.keys.map { |name| name.to_s.strip }.reject(&:empty?)
+
     generate_paginated_pages(site, "/tags", posts, per_page, {
       "layout" => "archive-taxonomy",
       "title" => "Tags",
-      "taxonomy_type" => "tags"
+      "taxonomy" => "tags",
+      "taxonomy_type" => "tags",
+      "tag_names" => tag_names
     })
   end
 
@@ -59,14 +78,46 @@ class ArchivePagesGenerator < Jekyll::Generator
     end
   end
 
+  def generate_archive_pagination_pages(site, type, taxonomies, per_page)
+    return unless taxonomies.is_a?(Hash)
+
+    taxonomies.each do |name, documents|
+      posts = visible_posts(Array(documents))
+      total_pages = [(posts.length.to_f / per_page).ceil, 1].max
+      next if total_pages <= 1
+
+      (2..total_pages).each do |page_number|
+        page_posts = posts[((page_number - 1) * per_page), per_page] || []
+        base_path = "/#{type}/#{slug(name)}"
+        page_data = {
+          "layout" => "archive-taxonomy",
+          "title" => name.to_s,
+          "taxonomy" => name.to_s,
+          "taxonomy_type" => type,
+          "posts" => page_posts,
+          "archive_page" => page_number,
+          "archive_per_page" => per_page,
+          "archive_total_posts" => posts.length,
+          "archive_total_pages" => total_pages,
+          "archive_previous" => pagination_path(base_path, page_number - 1),
+          "archive_next" => page_number < total_pages ? pagination_path(base_path, page_number + 1) : nil
+        }
+        page = ArchivePage.new(site, "#{base_path}/page/#{page_number}".sub(%r{^/}, ""), page_data)
+        page.content = ""
+        site.pages << page
+      end
+    end
+  end
+
   def generate_paginated_pages(site, base_path, posts, per_page, data)
+    posts = Array(posts).compact
     total_pages = [(posts.length.to_f / per_page).ceil, 1].max
 
     (1..total_pages).each do |page_number|
       page_posts = posts[((page_number - 1) * per_page), per_page] || []
       page_path = page_number == 1 ? base_path : "#{base_path}/page/#{page_number}"
       page_data = data.merge(
-        "posts" => page_posts,
+        "archive_posts" => page_posts,
         "pagination" => {
           "page" => page_number,
           "per_page" => per_page,
